0fd3961b681c6fdf0b644712cf4d2d4c8b6d5705
Pieter Daalder <pieterdaalder@Ginaz.fritz.box>
2020-07-21 12:30:34 +0200

    Fix #1 and hooked into admin_init.

--- sd-activate-plugin.php
+++ sd-activate-plugin.php
@@ -3,34 +3,45 @@
 /**
  * Plugin Name: SD Activate Plugin
  * Description: A MU plugin to activate another plugin
- * Version: 0.1
+ * Version: 1.0
  * Author: Pieter Daalder
  * License: GPLv2
  */
 
- // General settings go here
+ /**
+  * General settings for this plugin.
+  * $plugin_file : the file needed to activate the plugin.
+  * $notify : Boolean value, use true for sending an e-mail after the run of this plugin.
+  * $notify_mail : address used for notification if $notify is set to true.
+  */
 
- $plugin_file = "plugin-folder/plugin-file.php";
- $notify = true;
- $notify_mail = "your@email.com";
+function sd_activate_plugin() {
 
- // That's it! No more setup, just enjoy the ride.
+    $plugin_file = "plugin-folder/plugin-file.php";
+    $notify = true;
+    $notify_mail = "your@email.com";
 
- $result = activate_plugin( $plugin_file );
-if ( is_wp_error( $result ) ) {
-    $status = false;
-} else {
-    $status = true;
-}
-
-if ( $notify ) {
-    if ($status ){
-        $subject = 'Plugin activated';
-        $body = 'Plugin succesfully activated.';
+    $result = activate_plugin( $plugin_file );
+    if ( is_wp_error( $result ) ) {
+        $status = false;
     } else {
-        $subject = 'Plugin was not activated';
-        $body = 'Plugin not activated, something went wrong';
+        $status = true;
     }
-    wp_mail( $notify_mail, $subject, $body );
+
+    if ( $notify ) {
+        if ($status ){
+            $subject = 'Plugin activated';
+            $body = 'Plugin ' .$plugin_file .' succesfully activated.';
+        } else {
+            $subject = 'Plugin was not activated';
+            $body = 'Plugin ' .$plugin_file .' not activated, something went wrong';
+        }
+        wp_mail( $notify_mail, $subject, $body );
+    }
+
+    unlink(__FILE__);
 }
+
+add_action( 'admin_init', 'sd_activate_plugin' );
+
 ?>
